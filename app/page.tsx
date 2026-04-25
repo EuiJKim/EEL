327456761c0bbb4bd24e5411648c62199e3897c7
mi <mi@miui-MacBookPro.local>
2026-04-25 18:56:02 +0900

    revert hero logo to EEL only

--- app/page.tsx
+++ app/page.tsx
@@ -134,7 +134,7 @@ export default function Home() {
               WebkitTextStroke: '1px #000',
             }}
           >
-            STUDIO EEL
+            EEL
           </p>
 
           {/* Divider - full width */}
